a72e6f760e07e302c1516c4dadf7266eb9b26d9e
Dragonsjet <35273281+Dragonsjet@users.noreply.github.com>
2018-01-09 12:32:32 -0700

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour {
+    public Vector2 jump;
+    public float jumpForce = 2.0f;
+    Rigidbody2D rb;
+    public bool isGrounded;
+    Vector3 position;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //void OnCollision()
+
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position = this.transform.position;
+            position.x -= 0.2f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position = this.transform.position;
+            position.x += 0.2f;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        {
+            rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+            //isGrounded = false;
+        }
+    }
+}
---
+++ NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Rigidbody rb;
+    void Start()
+    {
+    rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, 10, 0);
+
+        }
+    }
+ 
+}
